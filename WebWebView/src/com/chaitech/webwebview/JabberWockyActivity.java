08841058b23befa699cdcb8414462c1277dbae97
luindc22203 <luindc22203@localhost>
2014-01-25 13:10:25 -0500

    with War of Worlds and Jabberwocky

--- WebWebView/src/com/chaitech/webwebview/JabberWockyActivity.java
+++ WebWebView/src/com/chaitech/webwebview/JabberWockyActivity.java
@@ -1,24 +1,24 @@
 package com.chaitech.webwebview;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
 import android.webkit.WebView;
 
-public class MainActivity extends Activity {
+public class JabberWockyActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		WebView webView = (WebView) findViewById(R.id.webView1);
-		webView.loadUrl("file:///android_asset/waroftheworlds.html");
+		webView.loadUrl("file:///android_asset/jabberwocky.html");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.jabber_wocky, menu);
 		return true;
 	}
 
